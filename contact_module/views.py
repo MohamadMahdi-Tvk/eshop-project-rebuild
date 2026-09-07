2911217d5e8b199a74a4cef8ea182977acd83433
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2026-09-07 23:55:13 +0330

    get and post methods in server

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 def contact_us_page(request):
+    if request.method == 'POST':
+        print(request.POST['email'])
+        print(request.POST['fullname'])
+        print(request.POST['subject'])
+        print(request.POST['message'])
+        return redirect(reverse('home_page'))
     return render(request, 'contact_module/contact_us_page.html', {})
