fd62fd3ac462b00729235c7a617829b6f8a6cbbc
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2026-09-14 22:08:38 +0330

    django forms - field configs

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -2,7 +2,13 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    full_name = forms.CharField()
-    email = forms.EmailField()
-    subject = forms.CharField()
-    text = forms.CharField()
+    full_name = forms.CharField(
+        label='نام و نام خانوادگی',
+        max_length=50,
+        error_messages={
+            'required': 'لطفا نام و نام خانوادگی خود را وارد کنید'
+        })
+
+    email = forms.EmailField(label='ایمیل', widget=forms.EmailInput)
+    subject = forms.CharField(label='عنوان')
+    text = forms.CharField(label='متن پیام', widget=forms.Textarea)
