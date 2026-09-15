744bc6e12499db599f6a6561534e3a2c84f58a92
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2026-09-16 01:03:42 +0330

    django forms - widget

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -6,9 +6,42 @@ class ContactUsForm(forms.Form):
         label='نام و نام خانوادگی',
         max_length=50,
         error_messages={
-            'required': 'لطفا نام و نام خانوادگی خود را وارد کنید'
+            'required': 'لطفا نام و نام خانوادگی خود را وارد کنید',
+            'max_length': 'نام و نام خانوادگی نمیتواند بیشتر از 50 کارکتر باشد'
+        },
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'نام و نام خانوادگی',
+            'autocomplete': 'name',
         })
+    )
 
-    email = forms.EmailField(label='ایمیل', widget=forms.EmailInput)
-    subject = forms.CharField(label='عنوان')
-    text = forms.CharField(label='متن پیام', widget=forms.Textarea)
+    email = forms.EmailField(
+        label='ایمیل',
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'ایمیل',
+            'autocomplete': 'email'
+        })
+    )
+
+    subject = forms.CharField(
+        label='عنوان',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'عنوان',
+            'autocomplete': 'off',
+
+        })
+    )
+
+    text = forms.CharField(
+        label='متن پیام',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'متن پیام',
+            'rows': '5',
+            'id': 'message',
+            'autocomplete': 'off'
+        })
+    )
